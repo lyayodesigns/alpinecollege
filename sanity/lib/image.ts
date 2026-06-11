cf14a22a80b5fc00496fc8bf04a473a67b336501
Karan Tiwari <tiwari.krn@gmail.com>
2026-06-11 09:37:06 +0545

    fix: derive ImageSource type from imageBuilder instead of removed @sanity/image-url export

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,8 +1,9 @@
 import createImageUrlBuilder from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url";
 import { dataset, projectId } from "../env";
 
 const imageBuilder = createImageUrlBuilder({ projectId, dataset });
 
-export const urlFor = (source: SanityImageSource) =>
+type ImageSource = Parameters<typeof imageBuilder.image>[0];
+
+export const urlFor = (source: ImageSource) =>
   imageBuilder.image(source);
